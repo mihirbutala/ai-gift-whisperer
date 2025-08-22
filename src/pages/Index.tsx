cda3f92f1bafafd33812450fddd9c2d1a446d6e6
mihir butala <34510219+mihirbutala@users.noreply.github.com>
2025-08-22 22:36:51 +0530

    Test Supabase connection

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import { Search, Upload } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Navigation } from "@/components/Navigation";
+import { SupabaseConnectionTest } from "@/components/SupabaseConnectionTest";
 import { Link } from "react-router-dom";
 
 const Index = () => {
@@ -37,6 +38,11 @@ const Index = () => {
               </Button>
             </Link>
           </div>
+        </div>
+        
+        {/* Supabase Connection Test - Remove this in production */}
+        <div className="mt-16 max-w-2xl mx-auto">
+          <SupabaseConnectionTest />
         </div>
       </section>
     </div>
